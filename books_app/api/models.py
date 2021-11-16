bdd1efcdd3be63a3bf956772f9c53d5614f76ab7
Daniel <vorobiov.daniel@gmail.com>
2021-11-16 21:12:35 +0200

    first commit

---
+++ books_app/api/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    original_title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    publisher = models.CharField(max_length=30)
+    year = models.IntegerField()
+    language = models.CharField(max_length=10)
+    original_language = models.CharField(max_length=10)
+    genres = models.CharField(max_length=30)
+    page_nr = models.IntegerField(max_length=4)
+    ISBN = models.CharField(max_length=20)
