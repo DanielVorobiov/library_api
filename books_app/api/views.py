e8e857891d3e66159a2ead6560b4e0cd33565e66
Daniel <vorobiov.daniel@gmail.com>
2021-11-21 23:09:20 +0200

    Page number Filters

--- books_app/api/views.py
+++ books_app/api/views.py
@@ -4,8 +4,14 @@ from .serializers import BookSerializer
 
 
 class BookList(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
+    
     serializer_class = BookSerializer
+    def get_queryset(self):
+        queryset = Book.objects.all()
+        page_nr = self.request.query_params.get('page_nr')
+        if page_nr is not None:
+            queryset = queryset.filter(page_nr= page_nr)
+        return queryset
 
 
 
